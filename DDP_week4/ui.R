006bad9089e7976282b4bf6e72d5048228912da2
yashBhardwaJ09 <iamyash7@gmail.com>
2021-11-25 11:53:21 +0530

    Final Commit

---
+++ DDP_week4/ui.R
@@ -0,0 +1,41 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# 
+
+#
+# Shiny App and Reproducible Pitch
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Height Pediction"),
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Prediction of the child's height based on gender and parent's height"),
+            helpText("Parameters:"),
+            sliderInput(inputId = "inFh",
+                        label = "Father's height (cm):",
+                        value = 150,
+                        min = 150,
+                        max = 220,
+                        step = 1),
+            sliderInput(inputId = "inMh",
+                        label = "Mother's height (cm):",
+                        value = 140,
+                        min = 140,
+                        max = 200,
+                        step = 1),
+            radioButtons(inputId = "inGen",
+                         label = "Child's gender: ",
+                         choices = c("Female"="female", "Male"="male"),
+                         inline = TRUE)
+        ),
+        
+        mainPanel(
+            htmlOutput("pText"),
+            htmlOutput("pred"),
+            plotOutput("Plot", width = "50%")
+        )
+    )
+))
